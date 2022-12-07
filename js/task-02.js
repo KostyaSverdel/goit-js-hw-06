35c8068c7544c98aabb1abb83ea08e7c87dfcde9
KostyaSverdel <109824269+KostyaSverdel@users.noreply.github.com>
2022-12-07 17:37:38 +0200

    2-task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
+"use strict";
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+let ulList = document.getElementById("ingredients");
+const foodIngredients = ingredients.forEach((ingredient) => {
+  let items = document.createElement("li");
+  items.innerHTML = ingredient;
+  ulList.append(items);
+});
+
+console.log(foodIngredients);
