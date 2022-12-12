332360ccf7a70027ca708bcb2cfdc39d9999ab3e
KostyaSverdel <109824269+KostyaSverdel@users.noreply.github.com>
2022-12-12 18:12:41 +0200

    another-update-task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,9 @@
 document.getElementById("validation-input").onblur = function () {
-  if (this.getAttribute("data-length") > this.value.length) {
-    this.classList.remove("valid");
-    this.classList.add("invalid");
-  } else {
+  if (this.getAttribute("data-length") == this.value.length) {
     this.classList.remove("invalid");
     this.classList.add("valid");
+  } else {
+    this.classList.remove("valid");
+    this.classList.add("invalid");
   }
 };
